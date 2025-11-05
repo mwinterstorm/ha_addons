87618f509aae2fa0fa7063dc0f409556d00efa7a
mwinterstorm <mark.wr@gmail.com>
2025-11-05 18:19:39 +1300

    Bump version to 0.0.14 in config.yaml and update import.sh to use the virtual environment Python interpreter

--- feijoa_statsEngine/rootfs/usr/local/share/feijoa/import.sh
+++ feijoa_statsEngine/rootfs/usr/local/share/feijoa/import.sh
@@ -8,7 +8,7 @@ USERS_CHUNK_SIZE="${USERS_CHUNK_SIZE:-1000}"
 
 echo "[import.sh] bucket=${S3_BUCKET} prefix=${S3_PREFIX} exec-db chunk=${CHUNK_SIZE} users-chunk=${USERS_CHUNK_SIZE}"
 
-exec python3 /usr/local/share/feijoa/s3_contributions_to_sql.py \
+exec /opt/feijoa-venv/bin/python3 /usr/local/share/feijoa/s3_contributions_to_sql.py \
   --bucket "${S3_BUCKET}" \
   --prefix "${S3_PREFIX}" \
   --exec-db \
